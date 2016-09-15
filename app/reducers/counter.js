3a1465adcd83bb676429fd6c736e5a78696fc886
atyenoria <eigotyoubunn30@gmail.com>
2016-09-15 23:26:56 +0900

    package name

--- app/reducers/counter.js
+++ app/reducers/counter.js
@@ -1,13 +1,13 @@
 import * as types from '../actions/actionTypes';
 
-import ActionCable from 'action-cable-node'
+import ActionCable from 'action-cable-react-native'
 
 const App = {}
 
 App.cable = ActionCable.createConsumer("ws://localhost:4000/cable")
 
 
-App.room = App.cable.subscriptions.create("RoomChannel",{
+App.room_chat = App.cable.subscriptions.create("RoomChannel",{
   connected: function() {
     console.log("connected: action cable")
   },
@@ -17,9 +17,11 @@ App.room = App.cable.subscriptions.create("RoomChannel",{
   received: function(data) {
     console.log("data:" + data)
   },
-  speak: function(message) {
+  chat_test: function(message) {
+    return this.perform('chat_test', { message: message, app: "chat"});
   }
 });
+
 
 const initialState = {
   count: 0
@@ -29,6 +31,7 @@ export default function counter(state = initialState, action = {}) {
   switch (action.type) {
     case types.INCREMENT:
     console.log("inc")
+    App.room_chat.chat_test("event.target.value");
       return {
         ...state,
         count: state.count + 1
